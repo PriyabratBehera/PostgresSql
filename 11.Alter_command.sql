3c3a96ad1f60989457cf81ba48b96773448f1c03
Priyabrat Behera <priyabrat.behera@floatr.in>
2024-04-09 23:28:51 +0530

    add some query

--- 11.Alter_command.sql
+++ 11.Alter_command.sql
@@ -14,3 +14,19 @@ ALTER TABLE table_name MODIFY column_name datatype NOT NULL;
 --Alter table to add unique constraint
 ALTER TABLE table_name
 ADD CONSTRAINT MyUniqueConstraint UNIQUE(column1, column2...);
+
+-- add phone and age columns to Customers table
+ALTER TABLE Customers
+ADD phone varchar(10), age int;
+
+-- rename column customer_id to c_id
+ALTER TABLE Customers
+RENAME COLUMN customer_id TO c_id;
+
+-- Modify the Data Type of a Column
+ALTER TABLE Customers
+ALTER COLUMN age TYPE VARCHAR(2);
+
+-- rename Customers table to New_customers
+ALTER TABLE Customers
+RENAME TO New_customers;
--- 20.Having claus.sql
+++ 20.Having claus.sql
@@ -5,3 +5,14 @@ SELECT
     COUNT(rating) AS review_count
 FROM full_reviews 
 GROUP BY title HAVING COUNT(rating) > 1;
+
+SELECT AggFunc(column), extra_columns
+FROM table
+GROUP BY target_column
+HAVING condition
+
+-- select customers with the same first name based on their age count 
+SELECT COUNT(age) AS Count, first_name
+FROM Customers
+GROUP BY first_name
+HAVING COUNT(age) > 1;
